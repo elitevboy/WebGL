ee4ac8f9fdb128f5523f3329e7eff78bb8aa371a
elitevboy <wangguangguigui@126.com>
2017-12-20 17:04:27 +0800

    Y

--- Demos/webar/scripts/bundleCCC.js
+++ Demos/webar/scripts/bundleCCC.js
@@ -438,7 +438,13 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
     }
 },
 
-
+function (a, b) {
+    a['exports'] = {
+        'host': 'https://webar.easyar.cn',
+        'appKey': '9e755d6e8cfa09e2666f22346e90c003',
+        'appSecret': 'SxuvFwydPP1iQqu06NWj3g8lCHWQ4y2eC0iByVF5iy4TDE9rCkwudOTNxi05hbgZ0JoNORoyFNgF0Z3wj55H4eqPNgrB7LFer0HoTlFoyf0sHBcDI1u5RfP8ZDiLJFw0'
+    };
+}
 
 ]));
 
