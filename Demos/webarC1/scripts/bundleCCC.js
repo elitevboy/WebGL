3fc8ec1feb3bc10c146992d1690f09f30d5a0652
elitevboy <wangguangguigui@126.com>
2017-12-28 15:59:33 +0800

    q

--- Demos/webarC1/scripts/bundleCCC.js
+++ Demos/webarC1/scripts/bundleCCC.js
@@ -160,7 +160,7 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
         });
 
         //开始按钮点击事件的方法l['onclick'] = function () {
-        l['onclick'] = function () {
+        l['click'] = function () {
         //var go = function () {
             l['style']['display'] = 'none';
             e['play']()['then'](function () {
