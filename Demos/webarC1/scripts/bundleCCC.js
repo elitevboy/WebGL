e2c27c7cbb2e616ca03f1288f909d8a3620d8fdd
elitevboy <wangguangguigui@126.com>
2017-12-28 15:54:20 +0800

    a

--- Demos/webarC1/scripts/bundleCCC.js
+++ Demos/webarC1/scripts/bundleCCC.js
@@ -152,7 +152,7 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
             B['style']['display'] = 'none';
             /*change*/
             //隐藏开始图标
-            //l['style']['display'] = 'block';
+            l['style']['display'] = 'block';
         })['catch'](function (a) {
             u['innerHTML'] = '载入失败';
             console['log']('\x20error', a);
@@ -337,9 +337,9 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
         };
 
 
-        setTimeout(function () {
-            l.click();
-        }, 0x8);
+        //setTimeout(function () {
+        //    l.click();
+        //}, 0x8);
 
 
         
