fb5a421fbb624e34a4833c7397e3a6c5c7c2ed1c
elitevboy <wangguangguigui@126.com>
2017-12-28 15:10:14 +0800

    u

--- Demos/webarC1/scripts/bundleCCC.js
+++ Demos/webarC1/scripts/bundleCCC.js
@@ -152,7 +152,7 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
             B['style']['display'] = 'none';
             /*change*/
             //隐藏开始图标
-            //l['style']['display'] = 'block';
+            l['style']['display'] = 'block';
         })['catch'](function (a) {
             u['innerHTML'] = '载入失败';
             console['log']('\x20error', a);
@@ -160,8 +160,8 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
         });
 
         //开始按钮点击事件的方法
-        //l['onclick'] = function () {
-        var go = function () {
+        l['onclick'] = function () {
+        //var go = function () {
             l['style']['display'] = 'none';
             e['play']()['then'](function () {
                 var E = new THREE[('Clock')]();
@@ -336,7 +336,7 @@ var _0x5149=function(b,f){b=b-0x0;var a=_0x1495[b];if(_0x5149['initialized']===u
             });
         };
 
-        go();
+        //go();
 
 
     }, ![]);
