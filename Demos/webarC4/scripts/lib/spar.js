54fc3a07be2bf9a170984ab36a02ca0fa30291d5
elitevboy <wangguangguigui@126.com>
2018-01-11 10:57:29 +0800

    h

--- Demos/webarC4/scripts/lib/spar.js
+++ Demos/webarC4/scripts/lib/spar.js
@@ -5102,75 +5102,75 @@ var _0x24fc=function(b,f){b=b-0x0;var a=_0x4fc2[b];if(_0x24fc['initialized']===u
         return a;
     }();
 
-    var w = function a(b, c) {
-        return {
-            'cameraFOV': 0x28,
-            'cameraAspect': b / c,
-            'cameraNear': 0.1,
-            'cameraFar': 0x3e8,
-            'cameraPosition': new THREE[('Vector3')](0x0, 0x0, 0x5)
-        };
-    };
+    //var w = function a(b, c) {
+    //    return {
+    //        'cameraFOV': 0x28,
+    //        'cameraAspect': b / c,
+    //        'cameraNear': 0.1,
+    //        'cameraFar': 0x3e8,
+    //        'cameraPosition': new THREE[('Vector3')](0x0, 0x0, 0x5)
+    //    };
+    //};
 
-    var x = function () {
-        function a(c, b) {
-            g(this, a);
-            this['_videoDevice'] = c;
-            var d = c['width'];
-            var e = c['height'];
-            b = Object['assign']({},w(d, e), b);
-            var k = this['_scene'] = new THREE['Scene']();
-            var j = this['_camera'] = new THREE['PerspectiveCamera'](b['cameraFOV'], b['cameraAspect'], b['cameraNear'], b['cameraFar']);
-            j['position']['copy'](b['cameraPosition']);
-            var f = this['_renderer'] = new THREE[('WebGLRenderer')]({
-                'alpha': !![]
-            });
-            f['setSize'](d, e);
-            f['autoClear'] = ![];
-            this['_containerElm'] = document['createElement']('div');
-            this['_containerElm']['style']['position'] = 'relative';
-            var h = this['_videoDevice']['videoElement'];
-            this['_containerElm']['appendChild'](h);
-            h['style']['position'] = 'absolute';
-            var i = this['_renderer']['domElement'];
-            this['_containerElm']['appendChild'](i);
-            i['style']['position'] = 'absolute';
-        }
-        h(a, [{
-            'key': 'start',
-            'value': function a(d) {
-                var b = this;
-                var c = function a() {
-                    requestAnimationFrame(function () {
-                        return a();
-                    });
-                    d();
-                    b['_renderer']['clear']();
-                    b['_renderer']['render'](b['_scene'], b['_camera']);
-                };
-                c();
-            }
-        },
-        {
-            'key': 'domElement',
-            'get': function a() {
-                return this['_containerElm'];
-            }
-        },
-        {
-            'key': 'scene',
-            'get': function a() {
-                return this['_scene'];
-            }
-        }]);
-        return a;
-    }();
+    //var x = function () {
+    //    function a(c, b) {
+    //        g(this, a);
+    //        this['_videoDevice'] = c;
+    //        var d = c['width'];
+    //        var e = c['height'];
+    //        b = Object['assign']({},w(d, e), b);
+    //        var k = this['_scene'] = new THREE['Scene']();
+    //        var j = this['_camera'] = new THREE['PerspectiveCamera'](b['cameraFOV'], b['cameraAspect'], b['cameraNear'], b['cameraFar']);
+    //        j['position']['copy'](b['cameraPosition']);
+    //        var f = this['_renderer'] = new THREE[('WebGLRenderer')]({
+    //            'alpha': !![]
+    //        });
+    //        f['setSize'](d, e);
+    //        f['autoClear'] = ![];
+    //        this['_containerElm'] = document['createElement']('div');
+    //        this['_containerElm']['style']['position'] = 'relative';
+    //        var h = this['_videoDevice']['videoElement'];
+    //        this['_containerElm']['appendChild'](h);
+    //        h['style']['position'] = 'absolute';
+    //        var i = this['_renderer']['domElement'];
+    //        this['_containerElm']['appendChild'](i);
+    //        i['style']['position'] = 'absolute';
+    //    }
+    //    h(a, [{
+    //        'key': 'start',
+    //        'value': function a(d) {
+    //            var b = this;
+    //            var c = function a() {
+    //                requestAnimationFrame(function () {
+    //                    return a();
+    //                });
+    //                d();
+    //                b['_renderer']['clear']();
+    //                b['_renderer']['render'](b['_scene'], b['_camera']);
+    //            };
+    //            c();
+    //        }
+    //    },
+    //    {
+    //        'key': 'domElement',
+    //        'get': function a() {
+    //            return this['_containerElm'];
+    //        }
+    //    },
+    //    {
+    //        'key': 'scene',
+    //        'get': function a() {
+    //            return this['_scene'];
+    //        }
+    //    }]);
+    //    return a;
+    //}();
 
     var D = {
         'VideoDevice': s,
         'CameraVideoSource': O,
         'Recognizer': J,
-        'THREERenderer': x,
+        //'THREERenderer': x,
         'browser': c,
         'checkBrowser': function a() {
             if (c['mobile'] || c['tablet']) {
