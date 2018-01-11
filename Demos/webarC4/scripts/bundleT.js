95e853fd1e148b93d042baeff8906294553edf8b
elitevboy <wangguangguigui@126.com>
2018-01-11 10:52:28 +0800

    g

--- Demos/webarC4/scripts/bundleT.js
+++ Demos/webarC4/scripts/bundleT.js
@@ -46,7 +46,88 @@ e['setVideoSource'](new SPAR[('CameraVideoSource')](C))['then'](function () {
         //var E = new THREE[('Clock')]();
 
 
-        var l = new SPAR[('THREERenderer')](e);
+
+
+
+        var w = function a(b, c) {
+            return {
+                'cameraFOV': 0x28,
+                'cameraAspect': b / c,
+                'cameraNear': 0.1,
+                'cameraFar': 0x3e8,
+                'cameraPosition': new THREE[('Vector3')](0x0, 0x0, 0x5)
+            };
+        };
+
+
+        var x = function () {
+            function a(c, b) {
+                g(this, a);
+                this['_videoDevice'] = c;
+                var d = c['width'];
+                var e = c['height'];
+                b = Object['assign']({}, w(d, e), b);
+                var k = this['_scene'] = new THREE['Scene']();
+                var j = this['_camera'] = new THREE['PerspectiveCamera'](b['cameraFOV'], b['cameraAspect'], b['cameraNear'], b['cameraFar']);
+                j['position']['copy'](b['cameraPosition']);
+                var f = this['_renderer'] = new THREE[('WebGLRenderer')]({
+                    'alpha': !![]
+                });
+                f['setSize'](d, e);
+                f['autoClear'] = ![];
+                this['_containerElm'] = document['createElement']('div');
+                this['_containerElm']['style']['position'] = 'relative';
+                var h = this['_videoDevice']['videoElement'];
+                this['_containerElm']['appendChild'](h);
+                h['style']['position'] = 'absolute';
+                var i = this['_renderer']['domElement'];
+                this['_containerElm']['appendChild'](i);
+                i['style']['position'] = 'absolute';
+            }
+            h(a, [{
+                'key': 'start',
+                'value': function a(d) {
+                    var b = this;
+                    var c = function a() {
+                        requestAnimationFrame(function () {
+                            return a();
+                        });
+                        d();
+                        b['_renderer']['clear']();
+                        b['_renderer']['render'](b['_scene'], b['_camera']);
+                    };
+                    c();
+                }
+            },
+            {
+                'key': 'domElement',
+                'get': function a() {
+                    return this['_containerElm'];
+                }
+            },
+            {
+                'key': 'scene',
+                'get': function a() {
+                    return this['_scene'];
+                }
+            }]);
+            return a;
+        }();
+
+
+
+
+
+
+
+
+
+
+
+
+        //var l = new SPAR[('THREERenderer')](e);
+
+        var l = x(e);
         m['appendChild'](l['domElement']);
         var C = (j - e['width']) / 0x2;
         var x = (g - e['height']) / 0x2;
